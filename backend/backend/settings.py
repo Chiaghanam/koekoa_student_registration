5ff8425ffe0bd45bc5719bd42cfad12179127c79
Amobi <leofazzy98@gmail.com>
2026-03-12 14:10:44 +0100

    updated CORS with frontend URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -166,4 +166,9 @@ STORAGES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS= [
+    'http://localhost:3000',
+    "http://localhost:5173",
+    "https://koekoa-student-registration-frontend.onrender.com",
+]
